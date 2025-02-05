94eba2e2998f3391be0a442cc032f9869f83efc9
PrimoCrypt <104681010+PrimoCrypt@users.noreply.github.com>
2025-02-05 23:20:23 +0100

    chore: removed the cors options

--- src/main.ts
+++ src/main.ts
@@ -5,16 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: [
-      'http://localhost:3000',
-      'https://number-classification-api-production-265d.up.railway.app',
-      'https://number-classification-api-2ysy.onrender.com',
-    ], // Add your allowed origins
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
-  });
+  app.enableCors();
 
   app.useGlobalPipes(new ValidationPipe());
 
